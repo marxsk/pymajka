5031552fd4e946d77de983f49336cfd4e8f3c2f9
Marek 'marx' Grac <mgrac@redhat.com>
2014-03-18 14:40:45 +0100

    Initial release with unit tests

---
+++ pymajka.py
@@ -0,0 +1,47 @@
+""" Python interface to morphological analyser majka """
+import pexpect
+import sys
+
+class Majka:
+	""" Python interface to morphological analyser majka
+
+		This class provides easy access to the morphological analyser. It is based on the command-line
+		interface so it should be rock stable. In order to start using this class you have to provide path
+		to binary of majka, dictionary path and information about used dictionary which is used to properly
+		group morphological data together.
+
+		During development, I was following a test-driven development what should help you to understand
+		basic usage. Just take a look at unittest in pymajka_test.py
+	"""
+	def __init__(self, command, dict_type = "lt"):
+		self.process = None
+		self.dict_type = dict_type
+		self.timeout = 1
+
+		self.conn = pexpect.spawn(command)
+
+	def get_raw(self, token):
+		""" Get raw output from majka """
+		self.conn.send(token + "\n")
+		self.conn.expect("\n", self.timeout)
+		self.conn.expect("\n", self.timeout)
+		output = self.conn.before.rstrip()
+
+		return output
+
+	def get_tuple(self, token):
+		""" Get tuples from majka output formated into pairs/triplets/... according to dict_type """
+		elements = self.get_raw(token).split(":")
+		if token != elements[0]:
+			print >> sys.stderr, "majka returned invalid output for token %s" % (token)
+			sys.exit(1)
+
+		if len(self.dict_type) == 1:
+			pairs = zip(elements[1::1])
+		elif len(self.dict_type) == 2:
+			pairs = zip(elements[1::2], elements[2::2])
+		else:
+			print >> sys.stderr, "we do not support more than pairs on output"
+			sys.exit(2)
+
+		return pairs
---
+++ pymajka_test.py
@@ -0,0 +1,93 @@
+#!/usr/bin/python
+# pylint: disable=R0904
+
+""" Unit test suite for pymajka """
+
+import unittest
+import pymajka
+import pexpect
+
+class TestPyMajka(unittest.TestCase):
+	""" Unit test suite for pymajka
+
+		It requires an access to majka and two selected dictionaries. Testing attempts to cover
+		all major situations when this class can be used. Including using several tokens in one
+		"connection"
+	"""
+	MAJKA_PATH = "majka/majka"
+	MAJKA_DICT = "majka/majka.w-lt"
+	MAJKA_DICT_LW = "majka/majka.l-w"
+
+	def test_constructor_basic(self):
+		""" Test basic constructor with default dictionary type """
+		pymajka.Majka("%s -f %s -p" % (self.MAJKA_PATH, self.MAJKA_DICT))
+
+	def test_constructor_wl(self):
+		""" Test basic constructor with given dictionary type """
+		pymajka.Majka("%s -f %s -p" % (self.MAJKA_PATH, self.MAJKA_DICT), "wl")
+
+	def test_run_noexec(self):
+		""" Test attempt to run file that exists but is not executable """
+		with self.assertRaises(pexpect.ExceptionPexpect):
+			pymajka.Majka(["/etc/passwd"])
+
+	def test_run_noexist(self):
+		""" Test attempt to run file that does not exist """
+		with self.assertRaises(pexpect.ExceptionPexpect):
+			pymajka.Majka(["/no/such/file"])
+
+	def test_raw_pes(self):
+		""" Check if we obtain same raw output as expected for token "pes" """
+		majka = pymajka.Majka("%s -f %s -p" % (self.MAJKA_PATH, self.MAJKA_DICT))
+		self.assertEquals("pes:pes:k1gMnSc1:peso:k1gNnPc2", majka.get_raw("pes"))
+
+	def test_tuple_lt_pes(self):
+		""" Check if we obtain formatted tuples as expected for token "pes" """
+		majka = pymajka.Majka("%s -f %s -p" % (self.MAJKA_PATH, self.MAJKA_DICT))
+		result = majka.get_tuple("pes")
+		self.assertEquals(2, len(result))
+		self.assertEquals("peso", result[1][0])
+		self.assertEquals("k1gNnPc2", result[1][1])
+
+	def test_raw_dub(self):
+		""" Check if we obtain same raw output as expected for token "dub" """
+		majka = pymajka.Majka("%s -f %s -p" % (self.MAJKA_PATH, self.MAJKA_DICT))
+		self.assertEquals("dub:dub:k1gInSc1:dub:k1gInSc4", majka.get_raw("dub"))
+
+	def test_tuple_lt_dub(self):
+		""" Check if we obtain formatted tuples as expected for token "pes" """
+		majka = pymajka.Majka("%s -f %s -p" % (self.MAJKA_PATH, self.MAJKA_DICT))
+		result = majka.get_tuple("dub")
+		self.assertEquals(2, len(result))
+		self.assertEquals("dub", result[0][0])
+		self.assertEquals("k1gInSc1", result[0][1])
+
+	def test_raw_xyz(self):
+		""" Test raw results of analyzing token that does not exists in Czech database """
+		majka = pymajka.Majka("%s -f %s -p" % (self.MAJKA_PATH, self.MAJKA_DICT))
+		self.assertEquals("xyz", majka.get_raw("xyz"))
+
+	def test_tuple_lt_xyz(self):
+		""" Test formatted tuples of analyzing token that does not exists in Czech database """
+		majka = pymajka.Majka("%s -f %s -p" % (self.MAJKA_PATH, self.MAJKA_DICT))
+		result = majka.get_tuple("xyz")
+		self.assertEquals(0, len(result))
+
+	def test_tuple_w_pes(self):
+		""" Check if we obtain formatted tuples as expected for token "pes" with different dictionary type"""
+		majka = pymajka.Majka("%s -f %s -p" % (self.MAJKA_PATH, self.MAJKA_DICT_LW), "w")
+		result = majka.get_tuple("pes")
+		self.assertEquals(15, len(result))
+		self.assertEquals("psa", result[1][0])
+
+	def test_multiple(self):
+		""" Test obtaining several analysis in one connection """
+		majka = pymajka.Majka("%s -f %s -p" % (self.MAJKA_PATH, self.MAJKA_DICT))
+		result = majka.get_tuple("xyz")
+		self.assertEquals(0, len(result))
+		result = majka.get_tuple("dub")
+		self.assertEquals(2, len(result))
+		self.assertEquals("k1gInSc1", result[0][1])
+
+if __name__ == '__main__':
+	unittest.main()
