9db0136cef3965d20b6b138af5a0c9a5aafc36e4
Marek 'marx' Grac <mgrac@redhat.com>
2015-08-14 11:55:31 +0200

    [build] Use distutils to install pymajka as standard module
    The installation of majka itself is not covered.

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python -tt
+
+from distutils.core import setup
+
+setup(
+	name='pymajka',
+	version='1.0',
+	description='Python interface to morphological analyser majka',
+	author='Marek marx Grac',
+	author_email='grac@mail.muni.cz',
+	url='http://github.com/marxsk/pymajka',
+	py_modules=['pymajka']
+)
